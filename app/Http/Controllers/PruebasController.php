5785860e1cc9f16607cfda0b78773d82bdd75935
maoknox <maoknox@maoknox>
2022-03-14 11:20:05 -0500

    cambio directorio

--- app/Http/Controllers/PruebasController.php
+++ app/Http/Controllers/PruebasController.php
@@ -10,7 +10,7 @@ class PruebasController extends Controller{
     	$response["status"]="success";
     	$response["msg"]="notas";
     	$myfile = fopen("/var/www/html/json-pruebas-estudiante.json", "r") or die("Unable to open file!");
-		$response["pruebas"]=json_decode(fread($myfile,filesize("/mnt/proyectos/bpgroup/proceso_software/json-pruebas-estudiante.json")),true);
+		$response["pruebas"]=json_decode(fread($myfile,filesize("/var/www/html/json-pruebas-estudiante.json")),true);
 		fclose($myfile);
     	return response()->json($response, 200);
     }
